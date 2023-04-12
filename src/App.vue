a62ffb6fd0f491b7a15a2f14f57ac88daf7a84db
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-12 07:29:19 +0545

    adding nav

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,23 @@
 
 <template>
   
-<router-view />
+<div class="bg-gray-100 h-full">
+  <header class="bg-white shadow h-16">
+
+    <router-link :to="{name:'home'}">
+        Home
+    </router-link>
+    <ul>
+      <li>
+        
+      </li>
+    </ul>
+
+  </header>
+  <main>
+    <router-view />
+  </main>
+</div>
 
 </template>
 
--- src/page/Home.vue
+++ src/page/Home.vue
@@ -8,20 +8,25 @@
     </router-link>
 
   </div>
+  <!-- <pre>{{ ingredients }}</pre> -->
 </div>
 
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
 import axiosClient from '../axiosClient';
 
+const ingredients = ref([])
+
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
 onMounted(async() => {
-  const response = await axiosClient.get('/list.php?c=list')
+  const response = await axiosClient.get('/list.php?i=list')
   
   console.log(response.data)
+
+  ingredients.value = response.data
 
   
 
