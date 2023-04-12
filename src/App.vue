3dbad9ac0e96b1cda2b27f15899bfb14cd7f0e45
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-12 07:45:43 +0545

    design navbar complete

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,23 @@
 <template>
   
 <div class="bg-gray-100 h-full">
-  <header class="bg-white shadow h-16">
+  <header class="bg-white shadow h-16 flex justify-between ">
 
-    <router-link :to="{name:'home'}">
+    <router-link :to="{name:'home'} " class="inline-flex px-5 items-center h-full">
         Home
     </router-link>
-    <ul>
-      <li>
-        
-      </li>
-    </ul>
+   <div class="flex justify-center gap-1 items-center">
+     <router-link to="/" class="  inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100 "  >
+        Search Meal
+      </router-link>
 
+      <router-link to="/" class=" inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
+        Search Meals by letters
+      </router-link>
+      <router-link to="/" class=" inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
+        Meals by Ingredients
+      </router-link>
+   </div>
   </header>
   <main>
     <router-view />
