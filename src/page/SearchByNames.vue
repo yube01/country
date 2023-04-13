3f938baad6cf0ee42076573587f8ba0d18736826
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-13 17:21:43 +0545

    search by name

--- src/page/SearchByNames.vue
+++ src/page/SearchByNames.vue
@@ -1,6 +1,25 @@
 <template>
-  By Names
+  <div class="p-8">
+    <input type="text" 
+    v-model="keyword"
+    @change="searchMeals"
+    class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals">
+  </div>
+  <div>
+    <pre>{{ meals }}</pre>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+import store from "../store"
+import { computed } from '@vue/reactivity';
+
+
+const meals = computed(()=> store.state.searchedMeals)
+
+
+const keyword = ref('')
+store.dispatch("searchedMeals",keyword.value)
 </script>
