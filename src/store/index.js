0d7d3046ff48c6fd302bddf92b286f907656517f
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-14 07:03:03 +0545

    mutation fixed

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 import { createStore } from "vuex";
 import state from "./state";
 import * as actions from "./actions";
-import * as mutation from "./mutation";
+import * as mutations from "./mutations";
 import * as getters from "./getters";
 
 const store = createStore({
   state,
   actions,
-  mutation,
+  mutations,
   getters,
 });
 
