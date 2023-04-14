f5b341c7bc83063cd02e27c83b169ac2d4676cae
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-14 07:11:12 +0545

    design to searchbynames

--- src/page/SearchByNames.vue
+++ src/page/SearchByNames.vue
@@ -5,8 +5,17 @@
     @change="searchMeals"
     class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals">
   </div>
-  <div>
-    <pre>{{ meals }}</pre>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-5">
+    <div v-for="meal of meals" :key="meal.idMeal">
+      <img :src="meal.strMealThumb" :alt="strMeal">
+      <h3>{{ meal.strMeal }}</h3>
+      <div>
+        <a :href="meal.strYoutube" target="_blank"> Youtube</a>
+        <router-link to="/" >
+          view
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
