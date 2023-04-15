3360025fb19795df7ca9b02af9279b58b518db9c
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-15 07:30:42 +0545

    design for measures complete

--- src/page/MealDetails.vue
+++ src/page/MealDetails.vue
@@ -29,8 +29,16 @@
         </template>
       </ul>
     </div>
-    <div class="text-2xl font-semibold mb-2">
-      <h2>Measures</h2>
+    <div>
+      <h2 class="text-2xl font-semibold mb-2">Measures</h2>
+      <ul>
+          <template v-for="(el, ind) of new Array(20)" >
+            <li v-if="meal[`strMeasure${ind + 1}`]">
+              {{ ind + 1 }}.{{ meal[`strMeasure${ind + 1}`] }}
+
+            </li>
+          </template>
+        </ul>
     </div>
 
    </div>
