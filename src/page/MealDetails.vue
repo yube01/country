6b5bfaf91dc728964e375d80c6dfbaeb8232ff5b
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-15 07:12:25 +0545

    category section completed

--- src/page/MealDetails.vue
+++ src/page/MealDetails.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
+  <div class="w-[800px] mx-auto">
     
-    <h1>{{ meal.strMeal }}</h1>
+    <h1 class="text-5xl font-bold mb-5">{{ meal.strMeal }}</h1>
     <img :src="meal.strMealThumb" :alt="meal.strMeal">
 
-    <div>
-      Category:{{ meal.strCategory }}
-    </div>
+   <div class="grid grid-cols-1 md:grid-cols-3 text-lg py-2">
+     <div>
+       <strong class="font-bold"> Category:</strong>{{ meal.strCategory }}
+      </div>
+      <div>
+         <strong class="font-bold">Area:</strong> {{ meal.strArea }}
+        </div>
+        <div>
+         <strong class="font-bold">Tags:</strong> {{ meal.strTags }}
+        </div>
+   </div>
     
   </div>
 </template>
