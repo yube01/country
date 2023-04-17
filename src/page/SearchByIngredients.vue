294249940ef6935f0621cfe4b7230932629e0259
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-17 07:21:26 +0545

    meals data

--- src/page/SearchByIngredients.vue
+++ src/page/SearchByIngredients.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-4 gap-5 p-5">
-    <MealItems v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
+  <div class="p-8 pb-0">
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">
+      Meals for {{ ingredient.strIngredient }}
+    </h1>
   </div>
+
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 
+import Meals from "../components/Meals.vue";
 import { computed } from "@vue/reactivity";
 import store from "../store";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+const ingredient = computed(() => store.state.ingredient);
 const meals = computed(() => {
   store.state.mealsByIngredient;
 });
