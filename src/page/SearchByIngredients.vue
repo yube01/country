50df0e46e70bed91ff7a05982a7bef283d1a954e
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-17 06:31:55 +0545

    structure and design added for ingredinets page

--- src/page/SearchByIngredients.vue
+++ src/page/SearchByIngredients.vue
@@ -1,5 +1,17 @@
 <template>
-  <pre>{{ ingredients }}</pre>
+  <div class="p-8">
+    <h1 class="text-5xl font-bold mb-6">Ingredients</h1>
+    <div
+      v-for="ingredient of ingredients"
+      :key="ingredient.id"
+      class="bg-white rounded mb-5 p-5 shadow"
+    >
+      <h3 class="text-2xl font-bold">
+        {{ ingredient.strIngredient }}
+      </h3>
+      <p>{{ ingredient.strDescription }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -9,7 +21,7 @@ import axiosClient from "../axiosClient";
 const ingredients = ref([]);
 onMounted(() => {
   axiosClient.get("/list.php?i=list").then(({ data }) => {
-    ingredients.value = data;
+    ingredients.value = data.meals;
   });
 });
 </script>
