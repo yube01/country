f5024d36d3eef4cecccc9457b6c3121d66efb22a
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-17 06:23:20 +0545

    axios in ingredients page

--- src/page/SearchByIngredients.vue
+++ src/page/SearchByIngredients.vue
@@ -1,6 +1,15 @@
 <template>
-By Ingredients
+  <pre>{{ ingredients }}</pre>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue";
+import axiosClient from "../axiosClient";
+
+const ingredients = ref([]);
+onMounted(() => {
+  axiosClient.get("/list.php?i=list").then(({ data }) => {
+    ingredients.value = data;
+  });
+});
 </script>
